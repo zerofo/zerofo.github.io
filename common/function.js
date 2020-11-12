33851bd2285ed8e18fa125d117d513abebe1524b
zerofo_tang <zerofo_tang@example.com>
2020-11-12 23:10:55 +0800

    add delay

--- common/function.js
+++ common/function.js
@@ -1,10 +1,10 @@
 function load_exploit_mira() {
     msgs.innerHTML="<div class='processing'></div><h1 style='font-size:25px;text-align:center;'> 正在加载 Exploit + Mira ...</h1>";
     ExploitMira=document.getElementById("oneclick").value;
-    console.log(ExploitMira);
     jbver = document.getElementById("jb_ver").value;
     if(jbver ==""){return 0;}
     LoadedMSG="使用"+ jbver + "破解成功 + Mira/Hen 已加载。<br>请等待 左上角, 出现2次 以上 跳窗 并等待跳窗结束再操作";
+
     setTimeout(function(){
         let script=document.createElement('script');
         script.src="./common/"+jbver+"_jailbreak.js";
@@ -12,6 +12,7 @@ function load_exploit_mira() {
 }
 
 function load_fan() {
+    setTimeout(function(){
     degree=document.getElementById('degree').value;
     msgs.innerHTML="<h1 style='font-size:25px;text-align:center;'> 正在加载 写入风扇数据 ...</h1>";
     LoadedMSG="已经成功 写入风扇数据"+degree;
@@ -26,9 +27,11 @@ function load_fan() {
     if(jbver ==""){jbver="new";}
     var binload=document.createElement('script');binload.src="./common/"+jbver+"_loadcode.js";
     document.getElementsByTagName('head')[0].appendChild(binload);
+},2500);
 }
 
 function load_binloader() {
+    setTimeout(function(){
     msgs.innerHTML="<h1 style='font-size:25px;text-align:center;'> 正在加载 Bin Loader ...</h1>";
     LoadedMSG="发送 bin格式插件(payload) 到端口 9021, <br>请等待 左上角, 出现2次 以上 跳窗并等待跳窗结束再操作";
     let loader=document.getElementById('loader').value=="def"? "mira": document.getElementById('loader').value;
@@ -39,8 +42,10 @@ function load_binloader() {
     if(jbver ==""){jbver="new";}
     let binload=document.createElement('script');binload.src="./common/"+jbver+"_loadcode.js";
     document.getElementsByTagName('head')[0].appendChild(binload);
+},2500);
 }
 function load_ftp() {
+    setTimeout(function(){
     msgs.innerHTML="<h1 style='font-size:25px;text-align:center;'> 正在加载 FTP ...</h1>";
 
     LoadedMSG="FTP 已加载 请等待 左上角,<br>出现2次 以上 跳窗并等待跳窗结束再操作";
@@ -56,8 +61,10 @@ function load_ftp() {
     if(jbver ==""){jbver="new";}
     let binload=document.createElement('script');binload.src="./common/"+jbver+"_loadcode.js";
     document.getElementsByTagName('head')[0].appendChild(binload);
+},2500);
 }
 function load_dumper() {
+    setTimeout(function(){
     msgs.innerHTML="<h1 style='font-size:25px;text-align:center;'> 正在加载 Dumper ...</h1>";
     LoadedMSG="Dumper 已加载 请等待 左上角,<br>出现2次 以上 跳窗并等待跳窗结束再操作";
     let script=document.createElement('script');
@@ -71,9 +78,11 @@ function load_dumper() {
     if(jbver ==""){jbver="new";}
     let binload=document.createElement('script');binload.src="./common/"+jbver+"_loadcode.js";
     document.getElementsByTagName('head')[0].appendChild(binload);
+},2500);
 }
 
 function load_linux() {
+    setTimeout(function(){
     msgs.innerHTML="<h1 style='font-size:25px;text-align:center;'> 正在加载 Linux ...</h1>";
 
     LoadedMSG="Linux 已加载 请等待 左上角,<br>出现2次 以上 跳窗并等待跳窗结束再操作";
@@ -88,9 +97,11 @@ function load_linux() {
     if(jbver ==""){jbver="new";}
     let binload=document.createElement('script');binload.src="./common/"+jbver+"_loadcode.js";
     document.getElementsByTagName('head')[0].appendChild(binload);
+},2500);
 }
 
 function load_app2usb() {
+    setTimeout(function(){
     msgs.innerHTML="<h1 style='font-size:25px;text-align:center;'> 正在加载 AppToUSB ...</h1>";
 
     LoadedMSG="AppToUSB 已加载 请等待 左上角,<br>出现2次 以上 跳窗并等待跳窗结束再操作";
@@ -105,9 +116,11 @@ function load_app2usb() {
     if(jbver ==""){jbver="new";}
     let binload=document.createElement('script');binload.src="./common/"+jbver+"_loadcode.js";
     document.getElementsByTagName('head')[0].appendChild(binload);
+},2500);
 }
 
 function load_kerneldump() {
+    setTimeout(function(){
     msgs.innerHTML="<h1 style='font-size:25px;text-align:center;'> 正在加载 内核 Dumper ...</h1>";
     LoadedMSG="内核 Dumper 已加载 请等待 左上角,<br>出现2次 以上 跳窗并等待跳窗结束再操作";
     let script=document.createElement('script');
@@ -121,10 +134,12 @@ function load_kerneldump() {
     if(jbver ==""){jbver="new";}
     let binload=document.createElement('script');binload.src="./common/"+jbver+"_loadcode.js";
     document.getElementsByTagName('head')[0].appendChild(binload);
+},2500);
 }
 
 
 function load_todex() {
+    setTimeout(function(){
     msgs.innerHTML="<h1 style='font-size:25px;text-align:center;'> 正在加载 toDEX ...</h1>";
     LoadedMSG="toDEX 已加载 请等待 左上角,<br>出现2次 以上 跳窗并等待跳窗结束再操作";
     let script=document.createElement('script');
@@ -138,9 +153,11 @@ function load_todex() {
     if(jbver ==""){jbver="new";}
     let binload=document.createElement('script');binload.src="./common/"+jbver+"_loadcode.js";
     document.getElementsByTagName('head')[0].appendChild(binload);
+},2500);
 }
 
 function load_webrte() {
+    setTimeout(function(){
     msgs.innerHTML="<h1 style='font-size:25px;text-align:center;'> 正在加载 WebRTE ...</h1>";
     LoadedMSG="WebRTE 已加载 请等待 左上角,<br>出现2次 以上 跳窗并等待跳窗结束再操作";
     let script=document.createElement('script');
@@ -154,9 +171,11 @@ function load_webrte() {
     if(jbver ==""){jbver="new";}
     let binload=document.createElement('script');binload.src="./common/"+jbver+"_loadcode.js";
     document.getElementsByTagName('head')[0].appendChild(binload);
+},2500);
 }
 
 function load_debug() {
+    setTimeout(function(){
     msgs.innerHTML="<h1 style='font-size:25px;text-align:center;'> 正在加载 Debug...</h1>";
     LoadedMSG="Ps4 Debug 已加载 请等待 左上角,<br>出现2次 以上 跳窗并等待跳窗结束再操作";
     let script=document.createElement('script');
@@ -170,9 +189,11 @@ function load_debug() {
     if(jbver ==""){jbver="new";}
     let binload=document.createElement('script');binload.src="./common/"+jbver+"_loadcode.js";
     document.getElementsByTagName('head')[0].appendChild(binload);
+},2500);
 }
 
 function load_historyblock() {
+    setTimeout(function(){
     msgs.innerHTML="<h1 style='font-size:25px;text-align:center;'> 正在加载禁用历史记录...</h1>";
 
     LoadedMSG="禁用历史记录已加载 请等待 左上角,<br>出现2次 以上 跳窗并等待跳窗结束再操作";
@@ -187,9 +208,11 @@ function load_historyblock() {
     if(jbver ==""){jbver="new";}
     let binload=document.createElement('script');binload.src="./common/"+jbver+"_loadcode.js";
     document.getElementsByTagName('head')[0].appendChild(binload);
+},2500);
 }
 
 function load_updatesdisable() {
+    setTimeout(function(){
     msgs.innerHTML="<h1 style='font-size:25px;text-align:center;'> 正在加载  屏蔽更新...</h1>";
 
     LoadedMSG="  屏蔽更新已加载 请等待 左上角,<br>出现2次 以上 跳窗并等待跳窗结束再操作";
@@ -204,9 +227,11 @@ function load_updatesdisable() {
     if(jbver ==""){jbver="new";}
     let binload=document.createElement('script');binload.src="./common/"+jbver+"_loadcode.js";
     document.getElementsByTagName('head')[0].appendChild(binload);
+},2500);
 }
 
 function load_updatesenable() {
+    setTimeout(function(){
     msgs.innerHTML="<h1 style='font-size:25px;text-align:center;'> 正在加载 开启更新...</h1>";
 
     LoadedMSG=" 开启更新已加载 请等待 左上角,<br>出现2次 以上 跳窗并等待跳窗结束再操作";
@@ -221,9 +246,11 @@ function load_updatesenable() {
     if(jbver ==""){jbver="new";}
     let binload=document.createElement('script');binload.src="./common/"+jbver+"_loadcode.js";
     document.getElementsByTagName('head')[0].appendChild(binload);
+},2500);
 }
 
 function load_dbbackup() {
+    setTimeout(function(){
     msgs.innerHTML="<h1 style='font-size:25px;text-align:center;'> 正在加载 存档备份 ...</h1>";
 
     LoadedMSG=" 存档备份 已加载 请等待 左上角,<br>出现2次 以上 跳窗并等待跳窗结束再操作";
@@ -238,9 +265,11 @@ function load_dbbackup() {
     if(jbver ==""){jbver="new";}
     let binload=document.createElement('script');binload.src="./common/"+jbver+"_loadcode.js";
     document.getElementsByTagName('head')[0].appendChild(binload);
+},2500);
 }
 
 function load_dbrestore() {
+    setTimeout(function(){
     msgs.innerHTML="<h1 style='font-size:25px;text-align:center;'> 正在加载 存档恢复 ...</h1>";
 
     LoadedMSG=" 存档恢复 已加载 请等待 左上角,<br>出现2次 以上 跳窗并等待跳窗结束再操作";
@@ -255,5 +284,6 @@ function load_dbrestore() {
     if(jbver ==""){jbver="new";}
     let binload=document.createElement('script');binload.src="./common/"+jbver+"_loadcode.js";
     document.getElementsByTagName('head')[0].appendChild(binload);
+},2500);
 }
 
