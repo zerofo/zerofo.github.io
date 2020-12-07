f2a024debe36124cdaa85644660f7884370e14f6
Your Name <tzf@dunlin.com>
2020-12-07 10:36:15 +0800

    fix btn and fix payload

--- common/function.js
+++ common/function.js
@@ -23,6 +23,27 @@ function auto_jb(show) {
         document.getElementById("autojb").innerHTML=" ON ";
     }
 }
+function change_oneclick(idx,name,val){
+    document.getElementById(idx).innerHTML=name;
+    document.getElementById(idx).value=val;
+    localStorage.setItem(idx+"Name", name);
+    localStorage.setItem(idx+"VAL", val);
+};
+function colorSwitch(btns){
+    var cmap=["BW","BG","WX"];
+    if(typeof(localStorage.getItem("themeVal"))=="undefined" || localStorage.getItem("themeVal") == null){
+        themeVal=0;
+        localStorage.setItem('themeVal',themeVal);
+    }
+    else{
+        themeVal=localStorage.getItem("themeVal");
+    }
+    if (btns != null)
+        {themeVal = Number(themeVal)+1;}
+    if (themeVal > 2){themeVal=0;}
+    document.documentElement.setAttribute('theme', cmap[themeVal]);
+    localStorage.setItem('themeVal',themeVal);
+};
 function load_script(name) {
     
     msgs.innerHTML="<div class='processing'></div><h1 style='font-size:25px;text-align:center;'> 正在加载 "+name+" ...</h1>";
@@ -39,6 +60,6 @@ function load_script(name) {
     ExploitMira=document.getElementById("oneclick").value;
     loaderjs.src="./pl/"+ExploitMira+"_loader.js";
     document.getElementsByTagName('head')[0].appendChild(loaderjs);
-    var binload=document.createElement('script');binload.src="./common/new_loadcode.js";
+    var binload=document.createElement('script');binload.src="./common/buildN_loadcode.js";
     document.getElementsByTagName('head')[0].appendChild(binload);
 }
