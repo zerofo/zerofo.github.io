4c713a573f3737e994278084f146c74582319ae5
Your Name <you@example.com>
2021-04-07 10:54:45 +0800

    new bin

--- common/function.js
+++ common/function.js
@@ -1,10 +1,3 @@
-/*var jbscript=document.createElement('script');
-jbscript.src="./common/buildN2_jailbreak.js";
-document.getElementsByTagName('head')[0].appendChild(jbscript);
-var loadbin = document.createElement('script');
-loadbin.src = "./common/buildN_loadcode.js";
-document.getElementsByTagName('head')[0].appendChild(loadbin);
-*/
 var loader_ = function(name,jb=0){
     var req=new XMLHttpRequest();
     req.responseType="arraybuffer";
@@ -16,18 +9,20 @@ var loader_ = function(name,jb=0){
             var tmp=new Uint8Array(req.response.byteLength);
             tmp.set(new Uint8Array(req.response),0);
             var payload=new Uint32Array(tmp);
-                window.mira_blob=malloc(65536);
-                write_mem(window.mira_blob, payload);
-            jb_status=0;
+                window.ldr_bin=malloc(65536);
+                write_mem(window.ldr_bin, payload);
+            //jb_status=0;
             if(jb){
                 setTimeout(function(){
-                    LoadedMSG = "破解成功 + Mira/Hen 已加载。请等待 左上角 跳窗结束再操作";
+                    LoadedMSG = "破解成功 + Mira/Hen 已加载。\n请等待 左上角 跳窗结束再操作";
                     jailbreak();
-                    if (jb_status) {loadcode();}
+                    //if (jb_status) {loadcode();}
+
                 }, 1300);
             }
         }};
 };
+
 var PLdr_ = function(name,jb=0,ld=1){
     var plreq=new XMLHttpRequest();
     plreq.responseType="arraybuffer";
@@ -37,22 +32,24 @@ var PLdr_ = function(name,jb=0,ld=1){
     plreq.onreadystatechange=function(){
         if(plreq.readyState==4)
         {
+            //jb_status=0;
+
             var pltmp=new Uint8Array(plreq.response.byteLength);
             pltmp.set(new Uint8Array(plreq.response),0);
             var payload_data=new Uint32Array(pltmp);
             for (var i = 0; i < payload_data.length; i++)var getlength = "0x" + plreq.response.byteLength.toString(16);
             setTimeout(function(){
                 if (ld)loader_("./pl/hen_loader.bin",0);
-                window.mira_blob_2_len=getlength;
-                window.mira_blob_2=malloc(window.mira_blob_2_len);
-                write_mem(window.mira_blob_2,payload_data);
+                window.pl_bin_len=getlength;
+                window.pl_bin=malloc(window.pl_bin_len);
+                write_mem(window.pl_bin,payload_data);
                 }, 400);
             if(jb){
                 setTimeout(function(){
-                    LoadedMSG= "破解成功 + Mira/Hen 已加载。请等待 左上角 跳窗结束再操作";
+                    LoadedMSG= "破解成功 + Mira/Hen 已加载。\n请等待 左上角 跳窗结束再操作";
                     //document.getElementsByTagName('head')[0].appendChild(jbscript);
                     jailbreak();
-                    if (jb_status) {loadcode();}
+                    //if (jb_status) {loadcode();}
                 }, 1300);
             }
         }};
@@ -60,9 +57,16 @@ var PLdr_ = function(name,jb=0,ld=1){
 function load_exploit_mira() {
     msgs.innerHTML="<div class='processing'></div><h1 style='font-size:25px;text-align:center;'> 正在加载 Exploit + Mira ...</h1>";
     var ExploitMira=document.getElementById("oneclick").value;
-    //parent.oneclick.style.display="none";
-    //parent.oneclick2.style.display="block";
-    if((ExploitMira=='zerofo') || (ExploitMira=='zerofo2'))loader_("./pl/"+ExploitMira+"_loader.bin",1);
+    fail_times = Number(localStorage.getItem("fail_times"))||0;
+    localStorage.setItem("fail_times", fail_times+1);
+    
+    if(ExploitMira=='zerofo') loader_("./pl/"+ExploitMira+"_loader.bin",1);
+    else if (ExploitMira == "binLoader_jb") {
+        setTimeout(function(){
+        LoadedMSG= "破解成功 已加载。\n请等待 左上角 跳窗结束再操作";
+        jailbreak();
+        }, 1300);
+    }
     else{
         PLdr_("./pl/"+ExploitMira+"_mira.bin",1);
     }
@@ -94,7 +98,7 @@ function colorSwitch(btns){
         localStorage.setItem('themeVal',themeVal);
     }
     if (btns)localStorage.setItem('Usetheme',1);
-    if (localStorage.getItem("Usetheme"))parent.fixedLayer.style.display="none";
+    if (localStorage.getItem("Usetheme"))window.fixedLayer.style.display="none";
     
     if (btns != null)themeVal = Number(themeVal)+1;
     if (themeVal > 2)themeVal=0;
@@ -104,13 +108,13 @@ function colorSwitch(btns){
 function load_script(name) {
     msgs.innerHTML="<div class='processing'></div><h1 style='font-size:25px;text-align:center;'> 正在加载 "+name+" ...</h1>";
     if (name == "fan"){
-    degree=parent.degree.value;
+    degree=window.degree.value;
     var pl=document.createElement('script');
     pl.src="./pl/pl_"+name+".js";
     document.getElementsByTagName('head')[0].appendChild(pl);
     }
-    else if (name != "binLoader") PLdr_("./pl/pl_"+name+".bin",0,0);
-    loader_("./pl/hen_loader.bin",0);
+    else if (name != "binLoader") loader_("./pl/pl_"+name+".bin",0,0);
+    //loader_("./pl/hen_loader.bin",0);
     LoadedMSG="已经成功载入 "+name+" 插件";
     setTimeout(function(){
     loadcode();
