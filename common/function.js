95844535e68ad77f3064545ddc03089391df3ca4
zerofo_tang <zerofo_tang@example.com>
2021-04-17 00:04:38 +0800

    new test

--- common/function.js
+++ common/function.js
@@ -26,11 +26,11 @@ var loader_ = async function(name,jb=0,pl=0){
         write_mem(window.ldr_bin, payload);
     }
     if(jb){
-        setTimeout(function(){
-            LoadedMSG = "破解成功 + Mira/Hen 已加载。\n请等待 左上角 跳窗结束再操作";
-            jailbreak();
-        }, 1300);
+    window.timeC.addEventListener('animationend', timedely);
+    window,timeC.style.animation='moving 1.3s alternate 1';
+    window,timeC.style.visibility='visible';
     }
+    return;
 }
 
 var PLdr_ = function(name,jb=0,ld=1){
@@ -39,42 +39,66 @@ var PLdr_ = function(name,jb=0,ld=1){
     loader_(name,0,1);
     }, 50);
     if(jb){
-        setTimeout(function(){
-            LoadedMSG= "破解成功 + Mira/Hen 已加载。\n请等待 左上角 跳窗结束再操作";
-            jailbreak();
-        }, 1300);
+    window.timeC.addEventListener('animationend', timedely);
+    window,timeC.style.animation='moving 1.3s alternate 1';
+    window,timeC.style.visibility='visible';
 
     }
+    return;
 }
 
+function timedely(func){
+    LoadedMSG= "破解成功 已加载。\n请等待 左上角 跳窗结束再操作";
+    var clicktime2=new Date();
+    console.log(clicktime2.getTime()-clicktime.getTime());
+    jailbreak();
+    window.timeC.removeEventListener('animationend', timedely)
+    window,timeC.style.visibility='hidden';
+    window,timeC.style.animation='';
+    window.ing.style.visibility='hidden';
+    
+}
 function load_exploit_mira() {
-    msgs.innerHTML="<div class='processing'></div><h1 style='font-size:25px;text-align:center;'> 正在加载 Exploit + Mira ...<br> 如果等待时间稍长请立即重启</h1>";
+    clicktime = new Date();
+
+    if ((clicktime.getTime()-startTime.getTime())<5000){
+        alert("请等待按钮 完全显示(共5s)");
+        return;
+    }
+    window.ing.style.visibility='visible';
+
+    msgs.innerHTML="<h1 style='font-size:25px;text-align:center;'> 正在加载 Exploit + Mira ...<br> 如果等待时间稍长请立即重启</h1>";
     var ExploitMira=document.getElementById("oneclick").value;
     fail_times = Number(localStorage.getItem("fail_times"))||0;
     localStorage.setItem("fail_times", fail_times+1);
     
     if(ExploitMira=='zerofo') loader_("./pl/"+ExploitMira+"_loader.bin",1);
     else if (ExploitMira == "binLoader_jb") {
-        let jb_time = setTimeout(function(){
-        LoadedMSG= "破解成功 已加载。\n请等待 左上角 跳窗结束再操作";
-        jailbreak();
-        }, 1300);
-        clearTimeout(jb_time);
+        
+    window.timeC.addEventListener('animationend', timedely);
+    window,timeC.style.animation='moving 1.3s alternate 1';
+    window,timeC.style.visibility='visible';
+
     }
     else{
         PLdr_("./pl/"+ExploitMira+"_mira.bin",1);
     }
+    startTime = new Date();
 
+    return;
 };
 function change_oneclick(idx,name,val){
     document.getElementById(idx).innerHTML=name;
     document.getElementById(idx).value=val;
     localStorage.setItem(idx+"Name", name);
     localStorage.setItem(idx+"VAL", val);
+    return;
 };
 
 function load_script(name) {
-    msgs.innerHTML="<div class='processing'></div><h1 style='font-size:25px;text-align:center;'> 正在加载 "+name+" ...</h1>";
+    window.ing.style.visibility='visible';
+
+    msgs.innerHTML="<h1 style='font-size:25px;text-align:center;'> 正在加载 "+name+" ...</h1>";
     if (name == "fan"){
     degree=window.degree.value;
     var pl=document.createElement('script');
@@ -85,5 +109,7 @@ function load_script(name) {
     LoadedMSG="已经成功载入 "+name+" 插件";
     setTimeout(function(){
     loadcode();
+    window.ing.style.visibility='hidden';
     }, 1300);
+    return;
 };
