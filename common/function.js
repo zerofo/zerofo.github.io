a341bcb51b789fb014733277823b29b2c6444a34
Your Name <you@example.com>
2022-01-14 22:13:31 +0800

    412

--- common/function.js
+++ common/function.js
@@ -18,30 +18,25 @@ var loader_ = async function(name,jb=0,pl=0,ldr_only=0){
     let payload=new Uint32Array(tmp);
 
     if (pl) {
-        //if(jb==0)
-        // await sleep(150);
-        //await sleep(500);
+        
         let getlength = payload.length;
         window.pl_bin_len=getlength;
         window.pl_bin=malloc(window.pl_bin_len);
+        if(name.includes('pl_fan')){
+            let degree=window.degree.value;
+            payload[7440] = degree;
+        }
         await write_mem(window.pl_bin,payload);
     }
     else{
-        // window.ldr_bin=malloc(150000);
-        // write_mem(window.ldr_bin, payload);
-
+  
         let getlength = payload.length;
         window.ldr_bin_len=getlength;
         window.ldr_bin=malloc(window.ldr_bin_len);
         await write_mem(window.ldr_bin,payload);
-        // await sleep(50);
 
     }
-    // if(name.includes("goldhen2b"))
-    //     await sleep(100);
-
     if(jb){
-    // await jailbreak();
 
     window.timeC.addEventListener('animationend', jb_time);
     window.timeC.style.animation='moving 1s alternate 1';
@@ -49,7 +44,6 @@ var loader_ = async function(name,jb=0,pl=0,ldr_only=0){
     window.timeC.style.visibility='visible';
     }
     if(ldr_only){
-        // loader_("./pl/hen_loader.bin",0);
     window.timeC.addEventListener('animationend', ldr_time);
     window.timeC.style.animation='moving 1s alternate 1';
     window.timeC.style.webkitanimation='moving 1s alternate 1';
@@ -67,7 +61,6 @@ var PLdr_ = function(name,jb=0,ld=1,ldr_only=0){
 
     if (ldr_only) 
     {
-        // loader_("./pl/hen_loader_net.bin",0);
     window.timeC.addEventListener('animationend', ldr_time);
     window.timeC.style.animation='moving 1s alternate 1';
     window.timeC.style.webkitanimation='moving 1s alternate 1';
@@ -81,13 +74,8 @@ var PLdr_ = function(name,jb=0,ld=1,ldr_only=0){
     }
     return;
 }
-// async function testjb(name){
-//     await jailbreak(name);
-//     // await read_ptr_at(0);
-// }
 async function jb_time(func){
     await jailbreak();
-    // await read_ptr_at(0);
 }
 async function ldr_time(func){
     await loadcode();
@@ -109,22 +97,8 @@ function load_exploit_mira() {
     document.getElementById("onekey").style.display='none';
     msgs.innerHTML="<h1 style='font-size:25px;text-align:center;'> 正在加载 Exploit + Hen ...<br> 时间太长请重启！！！</h1>";
     var ExploitMira=document.getElementById("oneclick").value;
-
-//     if(ExploitMira=='zerofo'){
-// // window.timeC.addEventListener('animationend', jb_time);
-// //     window.timeC.style.animation='moving 1s alternate 1';
-// //     window.timeC.style.webkitanimation='moving 1s alternate 1';
-// //     window.timeC.style.visibility='visible';
-//     }
     if(ExploitMira=='goldhen2b')loader_("./pl/goldhen2b2.bin",1);
-    // {
-    //     testjb("./pl/goldhen2b2.bin");
-    //     // loader_("./pl/goldhen2b2.bin",1);
-    //     // window.timeC.addEventListener('animationend', jb_time);
-    //     // window.timeC.style.animation='moving 1s alternate 1';
-    //     // window.timeC.style.webkitanimation='moving 1s alternate 1';
-    //     // window.timeC.style.visibility='visible';
-    // }
+
     else if (ExploitMira == "binLoader_jb") {
     msgs.innerHTML="<h1 style='font-size:25px;text-align:center;'> 已加载 binLoader 请发送 9020端口 左上角没有提示！！！</h1>";
     window.timeC.addEventListener('animationend', jb_time);
@@ -139,10 +113,7 @@ function load_exploit_mira() {
     window.timeC.style.webkitanimation='moving 1s alternate 1';
     window.timeC.style.visibility='visible';
     }
-    // else{
-    //     PLdr_("./pl/"+ExploitMira+"_mira.bin",1,1);
-    //     //loader_("./pl/"+ExploitMira+"_mira.bin",1,1);
-    // }
+ 
     startTime = new Date();
     return;
 };
@@ -163,19 +134,7 @@ function load_script(name) {
     timedelay=800;
     msgs.innerHTML="<h1 style='font-size:25px;text-align:center;'> 正在加载 "+name+" ...</h1>";
 
-    if (name == "fan"){
-
-    loader_("./pl/hen_loader_net.bin",0);
-    degree=window.degree.value;
-    var pl=document.createElement('script');
-    pl.src="./pl/pl_"+name+".js";
-    document.getElementsByTagName('head')[0].appendChild(pl);
-    setTimeout(function(){
-    LoadedMSG="已经成功载入 "+name+" 插件";
-    loadcode();
-    return;},timedelay);
-    }
-    else if(name.includes("binLoader")){
+    if(name.includes("binLoader")){
         if (name == "binLoader") {
         LoadedMSG="已经成功载入 loader 插件, 请发送插件到9021";
         loader_("./pl/hen_loader_net.bin",0,0,ldr_only=1);
